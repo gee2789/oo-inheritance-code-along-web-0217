3c0e6466ab0a0b89babfc1f54b7f06f31f90309e
gee2789 <geeyung.li@gmail.com>
2017-02-10 19:04:29 -0500

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,7 @@
-require_relative "./vehicle.rb"
+require_relative "./vehicle.rb" #require access to the other lib.rb file to pull super class
+
+class Car < Vehicle
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+end
